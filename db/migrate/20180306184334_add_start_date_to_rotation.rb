6630d749f98002d4909b7e8bbd646171a6fdf828
Dave Riddle <david@joyvuu.com>
2018-03-06 12:55:41 -0800

    Match Heroku's bundler version; update Rotation records on migration; bump packages

--- db/migrate/20180306184334_add_start_date_to_rotation.rb
+++ db/migrate/20180306184334_add_start_date_to_rotation.rb
@@ -1,5 +1,13 @@
 class AddStartDateToRotation < ActiveRecord::Migration[5.1]
-  def change
+  def up
     add_column :rotations, :start_date, :date
+
+    Rotation.find_each do |rotation|
+      rotation.set_start_date
+    end
+  end
+
+  def down
+    remove_column :rotations, :start_date
   end
 end
