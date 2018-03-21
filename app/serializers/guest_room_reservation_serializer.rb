1e7fff743da1d32cfbf77280cc549e32c7d44797
Dave Riddle <david@joyvuu.com>
2018-03-20 19:52:58 -0700

    annotate serializers

--- app/serializers/guest_room_reservation_serializer.rb
+++ app/serializers/guest_room_reservation_serializer.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: guest_room_reservations
+#
+#  id           :integer          not null, primary key
+#  community_id :integer          not null
+#  resident_id  :integer          not null
+#  date         :date             not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_guest_room_reservations_on_community_id  (community_id)
+#  index_guest_room_reservations_on_resident_id   (resident_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (community_id => communities.id)
+#  fk_rails_...  (resident_id => residents.id)
+#
+
 
 
 class GuestRoomReservationSerializer < ActiveModel::Serializer
