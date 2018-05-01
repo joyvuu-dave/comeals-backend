0973d4d70aa6b89896c1d6ec2bacc028241aa6c4
Dave Riddle <david@joyvuu.com>
2018-05-01 10:23:15 -0700

    Allow editing title from Admin page

--- app/admin/common_house_reservations.rb
+++ app/admin/common_house_reservations.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register CommonHouseReservation do
   menu label: "Common House"
 
   # STRONG PARAMS
-  permit_params :community_id, :resident_id, :start_date, :end_date
+  permit_params :community_id, :resident_id, :start_date, :end_date, :title
 
   # SCOPE
   scope_to :current_admin_user
@@ -14,6 +14,7 @@ ActiveAdmin.register CommonHouseReservation do
   form do |f|
     f.inputs do
       f.input :resident_id, as: :select, include_blank: false, label: 'Host', collection: Resident.where(community_id: current_admin_user.community_id).includes(:unit).adult.order('units.name ASC').map { |r| ["#{r.name} - #{r.unit.name}", r.id] }
+      f.input :title, input_html: { placeholder: "optional" }
       f.input :start_date
       f.input :end_date
       f.input :community_id, input_html: { value: current_admin_user.community_id }, as: :hidden
