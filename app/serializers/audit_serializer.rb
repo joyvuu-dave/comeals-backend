1417eb3b7d3b15637d3147a8abf35c7898a104f4
Dave Riddle <david@joyvuu.com>
2018-05-27 17:10:12 -0700

    Hack for displaying iPad history

--- app/serializers/audit_serializer.rb
+++ app/serializers/audit_serializer.rb
@@ -6,6 +6,15 @@ class AuditSerializer < ActiveModel::Serializer
              :display_time
 
   def user_name
+    # Hack for showing who signed up on iPad
+    if Rails.env.production?
+      return "Common House" if object.user&.id == 60
+    end
+
+    if Rails.env.development?
+      return "Common House" if object.user&.email == "bowen@email.com"
+    end
+
     resident_name_helper(object.user&.name)
   end
 
