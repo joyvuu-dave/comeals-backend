b5e07b1add2f518f05d43ad09037844268bd2535
Dave Riddle <david@joyvuu.com>
2018-07-05 14:32:36 -0700

    Fix iCal requests

--- app/controllers/api/v1/communities_controller.rb
+++ app/controllers/api/v1/communities_controller.rb
@@ -24,36 +24,31 @@ module Api
       def ical
         community = Community.find(params[:id])
 
-        respond_to do |format|
-          format.ics do
+        require 'icalendar/tzinfo'
+        tzid = community.timezone
+        tz = TZInfo::Timezone.get tzid
+        timezone = tz.ical_timezone DateTime.new 2017, 6, 1, 8, 0, 0
 
-            require 'icalendar/tzinfo'
-            tzid = community.timezone
-            tz = TZInfo::Timezone.get tzid
-            timezone = tz.ical_timezone DateTime.new 2017, 6, 1, 8, 0, 0
+        cal = Icalendar::Calendar.new
+        cal.add_timezone timezone
 
-            cal = Icalendar::Calendar.new
-            cal.add_timezone timezone
+        cal.x_wr_calname = community.name
 
-            cal.x_wr_calname = community.name
+        Meal.where(community_id: community.id).each do |meal|
+          event = Icalendar::Event.new
 
-            Meal.where(community_id: community.id).each do |meal|
-              event = Icalendar::Event.new
+          meal_date = meal.date
+          meal_date_time_start = DateTime.new(meal_date.year, meal_date.month, meal_date.day, meal_date.sunday? ? 18 : 19, 0)
+          meal_date_time_end = DateTime.new(meal_date.year, meal_date.month, meal_date.day, meal_date.sunday? ? 20 : 21, 0)
 
-              meal_date = meal.date
-              meal_date_time_start = DateTime.new(meal_date.year, meal_date.month, meal_date.day, meal_date.sunday? ? 18 : 19, 0)
-              meal_date_time_end = DateTime.new(meal_date.year, meal_date.month, meal_date.day, meal_date.sunday? ? 20 : 21, 0)
-
-              event.dtstart = Icalendar::Values::DateTime.new meal_date_time_start, 'tzid' => tzid
-              event.dtend = Icalendar::Values::DateTime.new meal_date_time_end, 'tzid' => tzid
-              event.summary = "Common Dinner"
-              event.description = "#{meal.description}\n\n\n\nSign up here: https://#{community.slug}.comeals#{top_level}/meals/#{meal.id}/edit"
-              cal.add_event(event)
-            end
-
-            render plain: cal.to_ical, content_type: "text/calendar"
-          end
+          event.dtstart = Icalendar::Values::DateTime.new meal_date_time_start, 'tzid' => tzid
+          event.dtend = Icalendar::Values::DateTime.new meal_date_time_end, 'tzid' => tzid
+          event.summary = "Common Dinner"
+          event.description = "#{meal.description}\n\n\n\nSign up here: https://#{community.slug}.comeals#{top_level}/meals/#{meal.id}/edit"
+          cal.add_event(event)
         end
+
+        render plain: cal.to_ical, content_type: "text/calendar"
       end
 
       # GET /api/v1/communities/:id/birthdays
--- app/controllers/api/v1/residents_controller.rb
+++ app/controllers/api/v1/residents_controller.rb
@@ -82,50 +82,45 @@ module Api
       def ical
         resident = Resident.find(params[:id])
 
-        respond_to do |format|
-          format.ics do
+        require 'icalendar/tzinfo'
+        tzid = resident.community.timezone
+        tz = TZInfo::Timezone.get tzid
+        timezone = tz.ical_timezone DateTime.new 2017, 6, 1, 8, 0, 0
 
-            require 'icalendar/tzinfo'
-            tzid = resident.community.timezone
-            tz = TZInfo::Timezone.get tzid
-            timezone = tz.ical_timezone DateTime.new 2017, 6, 1, 8, 0, 0
+        cal = Icalendar::Calendar.new
+        cal.add_timezone timezone
 
-            cal = Icalendar::Calendar.new
-            cal.add_timezone timezone
+        cal.x_wr_calname = "My #{resident.community.name}"
 
-            cal.x_wr_calname = "My #{resident.community.name}"
+        Bill.where(resident_id: resident.id).each do |bill|
+          event = Icalendar::Event.new
 
-            Bill.where(resident_id: resident.id).each do |bill|
-              event = Icalendar::Event.new
+          meal_date = bill.meal.date
+          meal_date_time_start = DateTime.new(meal_date.year, meal_date.month, meal_date.day, meal_date.sunday? ? 18 : 19, 0)
+          meal_date_time_end = DateTime.new(meal_date.year, meal_date.month, meal_date.day, meal_date.sunday? ? 20 : 21, 0)
 
-              meal_date = bill.meal.date
-              meal_date_time_start = DateTime.new(meal_date.year, meal_date.month, meal_date.day, meal_date.sunday? ? 18 : 19, 0)
-              meal_date_time_end = DateTime.new(meal_date.year, meal_date.month, meal_date.day, meal_date.sunday? ? 20 : 21, 0)
-
-              event.dtstart = Icalendar::Values::DateTime.new meal_date_time_start, 'tzid' => tzid
-              event.dtend = Icalendar::Values::DateTime.new meal_date_time_end, 'tzid' => tzid
-              event.summary = "Cook Common Dinner"
-              event.description = "#{bill.meal.description}\n\n\n\nView here: https://#{bill.community.slug}.comeals#{top_level}/meals/#{bill.meal.id}/edit"
-              cal.add_event(event)
-            end
-
-            MealResident.where(resident_id: resident.id).each do |mr|
-              event = Icalendar::Event.new
-
-              meal_date = mr.meal.date
-              meal_date_time_start = DateTime.new(meal_date.year, meal_date.month, meal_date.day, meal_date.sunday? ? 18 : 19, 0)
-              meal_date_time_end = DateTime.new(meal_date.year, meal_date.month, meal_date.day, meal_date.sunday? ? 20 : 21, 0)
-
-              event.dtstart = Icalendar::Values::DateTime.new meal_date_time_start, 'tzid' => tzid
-              event.dtend = Icalendar::Values::DateTime.new meal_date_time_end, 'tzid' => tzid
-              event.summary = "Attend Common Dinner"
-              event.description = "#{mr.meal.description}\n\n\n\nView here: https://#{mr.community.slug}.comeals#{top_level}/meals/#{mr.meal.id}/edit"
-              cal.add_event(event) unless Bill.joins(:meal).where(resident_id: resident.id).where("meals.date = ?", mr.meal.date).present?
-            end
-
-            render plain: cal.to_ical, content_type: "text/calendar"
-          end
+          event.dtstart = Icalendar::Values::DateTime.new meal_date_time_start, 'tzid' => tzid
+          event.dtend = Icalendar::Values::DateTime.new meal_date_time_end, 'tzid' => tzid
+          event.summary = "Cook Common Dinner"
+          event.description = "#{bill.meal.description}\n\n\n\nView here: https://#{bill.community.slug}.comeals#{top_level}/meals/#{bill.meal.id}/edit"
+          cal.add_event(event)
         end
+
+        MealResident.where(resident_id: resident.id).each do |mr|
+          event = Icalendar::Event.new
+
+          meal_date = mr.meal.date
+          meal_date_time_start = DateTime.new(meal_date.year, meal_date.month, meal_date.day, meal_date.sunday? ? 18 : 19, 0)
+          meal_date_time_end = DateTime.new(meal_date.year, meal_date.month, meal_date.day, meal_date.sunday? ? 20 : 21, 0)
+
+          event.dtstart = Icalendar::Values::DateTime.new meal_date_time_start, 'tzid' => tzid
+          event.dtend = Icalendar::Values::DateTime.new meal_date_time_end, 'tzid' => tzid
+          event.summary = "Attend Common Dinner"
+          event.description = "#{mr.meal.description}\n\n\n\nView here: https://#{mr.community.slug}.comeals#{top_level}/meals/#{mr.meal.id}/edit"
+          cal.add_event(event) unless Bill.joins(:meal).where(resident_id: resident.id).where("meals.date = ?", mr.meal.date).present?
+        end
+
+        render plain: cal.to_ical, content_type: "text/calendar"
       end
 
       private
