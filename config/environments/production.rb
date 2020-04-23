f96cea597031b1a75cbd23a7ea3c22ae24cbc6b2
Dave Riddle <david@joyvuu.com>
2020-04-23 13:50:44 -0700

    fix error when sending email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # ActiveAdmin
+  config.action_mailer.default_url_options = { host: 'admin.comeals.com' }
+
   # Sendgrid
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
