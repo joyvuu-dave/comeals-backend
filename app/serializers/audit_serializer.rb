a0cdc4b6fd35b267ed60ddc36bdc6bc8ccba40b9
Dave Riddle <driddle@vmware.com>
2021-10-26 09:50:31 -0700

    Remove iPad hack

--- app/serializers/audit_serializer.rb
+++ app/serializers/audit_serializer.rb
@@ -7,13 +7,13 @@ class AuditSerializer < ActiveModel::Serializer
 
   def user_name
     # Hack for showing who signed up on iPad
-    if Rails.env.production?
-      return "Common House" if object.user&.id == 60
-    end
+    # if Rails.env.production?
+    #   return "Common House" if object.user&.id == 60
+    # end
 
-    if Rails.env.development?
-      return "Common House" if object.user&.email == "bowen@email.com"
-    end
+    # if Rails.env.development?
+    #   return "Common House" if object.user&.email == "bowen@email.com"
+    # end
 
     resident_name_helper(object.user&.name)
   end
